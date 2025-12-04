b0310f47458528d5f7f5d98a0a13f1516113c5ba
Seweryn Tasior <seweryn.tasior@op.pl>
2025-12-04 02:30:57 +0100

    Init backend spring boot

---
+++ backend-spring-boot/src/main/java/pl/edu/agh/to/backendspringboot/BackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.backendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringBootApplication.class, args);
+    }
+
+}
---
+++ backend-spring-boot/src/test/java/pl/edu/agh/to/backendspringboot/BackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.backendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
