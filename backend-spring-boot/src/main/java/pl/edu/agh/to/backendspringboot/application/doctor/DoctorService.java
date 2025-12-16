6e9cd5b1fd0bd9666ee7fdd3124feceb7db115b8
Bartosz Ludwin <93099232+FalcO0O@users.noreply.github.com>
2025-12-16 01:55:14 +0100

    add javadocs

--- backend-spring-boot/src/main/java/pl/edu/agh/to/backendspringboot/application/doctor/DoctorService.java
+++ backend-spring-boot/src/main/java/pl/edu/agh/to/backendspringboot/application/doctor/DoctorService.java
@@ -9,27 +9,63 @@ import pl.edu.agh.to.backendspringboot.domain.doctor.exception.DoctorNotFoundExc
 
 import java.util.List;
 
+/**
+ * Serwis realizujący logikę biznesową związaną z zarządzaniem lekarzami.
+ * Odpowiada za komunikację między kontrolerem a warstwą danych (repozytorium),
+ * a także za mapowanie obiektów domenowych na DTO.
+ */
 @Service
 public class DoctorService {
     private final DoctorRepository doctorRepository;
 
+    /**
+     * Konstruktor serwisu wstrzykujący zależność repozytorium.
+     *
+     * @param doctorRepository Repozytorium umożliwiające operacje na bazie danych lekarzy.
+     */
     public DoctorService(DoctorRepository doctorRepository) {
         this.doctorRepository = doctorRepository;
     }
 
+    /**
+     * Tworzy i zapisuje nowego lekarza w systemie.
+     * Metoda konwertuje obiekt żądania (DTO) na encję domenową przed zapisem.
+     *
+     * @param doctorRequest Obiekt zawierający dane niezbędne do utworzenia lekarza.
+     */
     public void addDoctor(DoctorRequest doctorRequest){
         doctorRepository.save(DoctorRequest.toEntity(doctorRequest));
     }
 
+    /**
+     * Pobiera listę wszystkich lekarzy w formacie skróconym.
+     * Dane są pobierane z repozytorium, a następnie mapowane na obiekty {@link DoctorBriefResponse}.
+     *
+     * @return Lista skróconych informacji o lekarzach.
+     */
     public List<DoctorBriefResponse> getDoctors() {
         return doctorRepository.findDoctorsBrief().stream().map(DoctorBriefResponse::from).toList();
     }
 
+    /**
+     * Pobiera szczegółowe informacje o lekarzu na podstawie jego identyfikatora.
+     *
+     * @param id Unikalny identyfikator lekarza.
+     * @return Obiekt {@link DoctorInfoResponse} zawierający pełne dane lekarza.
+     * @throws DoctorNotFoundException jeśli lekarz o podanym identyfikatorze nie zostanie znaleziony.
+     */
     public DoctorInfoResponse getDoctorInfoById(Integer id) {
         return doctorRepository.findDoctorInfoById(id).map(DoctorInfoResponse::from)
                 .orElseThrow(()->new DoctorNotFoundException("Doctor with id "+id+" not found"));
     }
 
+    /**
+     * Usuwa lekarza z systemu na podstawie jego identyfikatora.
+     * Przed usunięciem następuje weryfikacja, czy lekarz o danym ID istnieje.
+     *
+     * @param id Unikalny identyfikator lekarza do usunięcia.
+     * @throws DoctorNotFoundException jeśli lekarz o podanym identyfikatorze nie istnieje.
+     */
     public void deleteDoctorById(Integer id) {
         if (!doctorRepository.existsById(id)) {
             throw new DoctorNotFoundException("Doctor with id " + id + " not found");
--- backend-spring-boot/src/main/java/pl/edu/agh/to/backendspringboot/presentation/doctor/DoctorController.java
+++ backend-spring-boot/src/main/java/pl/edu/agh/to/backendspringboot/presentation/doctor/DoctorController.java
@@ -13,17 +13,34 @@ import pl.edu.agh.to.backendspringboot.domain.doctor.exception.DoctorNotFoundExc
 import pl.edu.agh.to.backendspringboot.domain.doctor.exception.InvalidMedicalSpecialization;
 
 import java.util.List;
-
+/**
+ * Kontroler REST obsługujący operacje związane z zarządzaniem lekarzami.
+ * Udostępnia endpointy do tworzenia, pobierania i usuwania lekarzy.
+ */
 @RestController
 @RequestMapping("doctors")
 @CrossOrigin(origins = "http://localhost:3000")
 public class DoctorController {
     private final DoctorService doctorService;
-
+    
+    /**
+     * Konstruktor kontrolera wstrzykujący zależność serwisu lekarzy.
+     *
+     * @param doctorService Serwis zawierający logikę biznesową dotyczącą lekarzy.
+     */
     public DoctorController(DoctorService doctorService) {
         this.doctorService = doctorService;
     }
 
+    /**
+     * Dodaje nowego lekarza do systemu.
+     * <p>
+     * Metoda waliduje poprawność danych wejściowych. Jeśli specjalizacja medyczna
+     * jest nieprawidłowa, zwracany jest błąd 400 Bad Request.
+     *
+     * @param doctorRequest Obiekt DTO zawierający dane wymagane do utworzenia lekarza.
+     * @throws ResponseStatusException (HttpStatus.BAD_REQUEST) jeśli podana specjalizacja jest nieprawidłowa.
+     */
     @PostMapping
     public void addDoctor(@Valid @RequestBody DoctorRequest doctorRequest){
         try {
@@ -33,21 +50,39 @@ public class DoctorController {
         }
     }
 
+    /**
+     * Pobiera listę wszystkich lekarzy dostępnych w systemie.
+     * Zwraca dane w formie skróconej (Brief).
+     *
+     * @return Lista obiektów {@link DoctorBriefResponse} reprezentujących lekarzy.
+     */
     @GetMapping
     public List<DoctorBriefResponse> getDoctors(){
         return doctorService.getDoctors();
     }
 
+    /**
+     * Pobiera szczegółowe informacje o konkretnym lekarzu na podstawie jego identyfikatora.
+     *
+     * @param id Unikalny identyfikator lekarza.
+     * @return Obiekt {@link DoctorInfoResponse} zawierający szczegółowe dane lekarza.
+     * @throws ResponseStatusException (HttpStatus.NOT_FOUND) jeśli lekarz o podanym ID nie istnieje.
+     */
     @GetMapping("/{id}")
     public DoctorInfoResponse getDoctorById(@PathVariable Integer id) {
         try {
             return doctorService.getDoctorInfoById(id);
         } catch (DoctorNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getLocalizedMessage());
-
         }
     }
 
+    /**
+     * Usuwa lekarza z systemu na podstawie jego identyfikatora.
+     *
+     * @param id Unikalny identyfikator lekarza do usunięcia.
+     * @throws ResponseStatusException (HttpStatus.NOT_FOUND) jeśli lekarz o podanym ID nie istnieje.
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteDoctorById(@PathVariable Integer id) {
